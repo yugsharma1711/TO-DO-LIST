2ef921415d0e741c6f3aad59dbb91e902c76e3c2
yugsharma1711 <imperialyug0805@gmail.com>
2022-03-26 12:08:11 +0530

    Added the poopup functionality

---
+++ script.js
@@ -0,0 +1,6 @@
+create_task_btn = document.getElementById("create_new_btn")
+create_task_btn.addEventListener("click", function(){
+    popup_container = document.getElementById("create_new_task")
+    popup_container.innerHTML = 
+    "<h1> Hello </h1>"
+})
