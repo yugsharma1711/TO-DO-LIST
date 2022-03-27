823a142f6cdecc455beda0c829c6b460dc1b32da
yugsharma1711 <imperialyug0805@gmail.com>
2022-03-27 05:59:17 +0530

    Added functionaity to task creater

--- script.js
+++ script.js
@@ -8,3 +8,9 @@ close_task_btn.addEventListener('click', function(){
     popup = document.getElementById('create_new_task')
     popup.style.display = 'none'
 })
+save_task_btn = document.getElementById('save_task')
+save_task_btn.addEventListener('click', function(){
+    task = document.getElementById('task_entered').value
+    time =  document.getElementById('time_entered').value
+    document.getElementById('task_list').innerHTML += '<li> ${task} ${time} </li>'
+})
