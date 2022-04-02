9c3090e3bb23527a03dc00e7229ec14778b3e448
yugsharma1711 <imperialyug0805@gmail.com>
2022-04-02 14:29:39 +0530

    Replaced innerHTML with create element

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 create_task_btn = document.getElementById("create_new_btn")
+let counter = 0
 create_task_btn.addEventListener("click", function(){
     popup = document.getElementById('create_new_task')
     popup.style.display = 'flex'
@@ -12,5 +13,11 @@ save_task_btn = document.getElementById('save_task')
 save_task_btn.addEventListener('click', function(){
     task = document.getElementById('task_entered').value
     time =  document.getElementById('time_entered').value
-    document.getElementById('task_list').innerHTML += '<li> ${task} ${time} </li>'
+    let value = 'Task : '+ task + ' ' + 'Deadline : ' + time
+    let task_container = document.getElementById('container_task')
+    let task_name = counter
+    let newDiv = document.createElement('div') 
+    let text_content = document.createTextNode(value)
+    newDiv.appendChild(text_content)
+    task_container.appendChild(newDiv)
 })
